a51f4c673b43567b18a24da0f34b4f0b65d6591d
danhkhai07 <aliveomg@gmail.com>
2026-07-17 13:34:00 +0700

    fix: remove previous template code

--- mock-info-service/internal/api/dto/get_response.go
+++ mock-info-service/internal/api/dto/get_response.go
@@ -1,39 +1,5 @@
 package dto
 
 import (
-	"agent/internal/domain"
+
 )
-
-type SessionMetadata struct {
-	ID string `json:"id"`
-	OwnerID string `json:"owner_id"`
-	Title string `json:"title"`
-}
-
-type GetAllSessionsResponse struct {
-	Sessions []SessionMetadata `json:"sessions"`
-}
-
-func NewGetAllSessionsResponse(sessions []domain.Session) *GetAllSessionsResponse {
-	response := GetAllSessionsResponse{
-		Sessions: make([]SessionMetadata, 0),
-	}
-	for _, session := range sessions {
-		metadata := SessionMetadata {
-			ID: session.ID,
-			Title: session.Title,
-		}
-		response.Sessions = append(response.Sessions, metadata)
-	}
-	return &response
-}
-
-type GetSessionResponse struct {
-	Session domain.Session
-}
-
-func NewGetSessionReponse(session domain.Session) *GetSessionResponse {
-	return &GetSessionResponse{
-		Session: session,
-	}
-}
--- mock-info-service/internal/api/routes.go
+++ mock-info-service/internal/api/routes.go
@@ -1,25 +1,11 @@
 package api
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
-
-func resolveStaticDir() string {
-	if configuredDir := os.Getenv("AGENT_STATIC_DIR"); configuredDir != "" {
-		return configuredDir
-	}
-	log.Fatalf("Error: AGENT_STATIC_DIR not configured in environment variables\n")
-	return ""
-}
 
 func addRoutes(server *Server) {
 	mux := http.NewServeMux()
-
-	fs := http.FileServer(http.Dir(resolveStaticDir()))
-
-
 
 	server.httpServer.Handler = mux
 }
